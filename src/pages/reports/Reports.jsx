f3b445314cb8479645fd87f017a7b7fdd68243c5
Aravinthan <naveenkumar@Stephens-MacBook-Air.local>
2024-02-02 12:56:36 +0530

    update report

--- src/pages/reports/Reports.jsx
+++ src/pages/reports/Reports.jsx
@@ -30,9 +30,12 @@ const Reports = () => {
   const [Full, setFull] = useState(["null"]);
   const [selectBusbar, setSelectBusbar] = useState("sensorModel6");
 
+  const [loading, setLoading] = useState();
+
   // const [isBusBarClicked, setIsBusBarClicked] = useState(false);
 
-  const [infoReport, setInfoReport] = useState([]);
+  const [infoReport6, setInfoReport6] = useState([]);
+  const [infoReport10, setInfoReport10] = useState([]);
   const [errorReport, setErrorReport] = useState();
 
   const [FullData, setFullData] = useState([]);
@@ -76,72 +79,115 @@ const Reports = () => {
   }, [Full]);
 
   useEffect(() => {
-    fetchDataBusbar6();
+    const fetchData = async () => {
+      try {
+        var url;
+      if (StartTime != null) {
+        url = `https://vedanta.xyma.live/sensor/gettime?start=${StartTime}&end=${EndTime}&busbar=sensorModel6`;
+      } else {
+        url = `https://vedanta.xyma.live/sensor/getfullBackupsensorModel6`;
+      }
+        console.log(url);
+        const response = await fetch(url);
+        const infoVal = await response.json();
+        setInfoReport6(infoVal);
+        // setIsBusBarClicked(false);
+      //   setTimeout(() => {
+      //     handleDownload6();
+      // }, 3000);
+      } catch (error) {
+        setErrorReport(error);
+      }
+    };
     // const interval = setInterval(() => {
-    //   fetchData();
+      fetchData();
     // }, 1000);
     // return () => {
     //   clearInterval(interval);
     // }
-  }, [StartTime, EndTime, selectBusbar]);
+  }, []);
 
-  // const fetchDataBusbar6 = async () => {
-  //   try {
-  //     var url;
-  //   if (StartTime != null) {
-  //     url = `https://vedanta.xyma.live/sensor/gettime?start=${StartTime}&end=${EndTime}&busbar=sensorModel6`;
-  //   } else {
-  //     url = `https://vedanta.xyma.live/sensor/getfullBackupsensorModel6`;
-  //   }
-  //     console.log(url);
-  //     const response = await fetch(url);
-  //     const infoVal = await response.json();
-  //     setInfoReport(infoVal);
-  //     // setIsBusBarClicked(false);
-  //   } catch (error) {
-  //     setErrorReport(error);
-  //   }
-  // };
-
-  // const fetchDataBusbar6 = async () => {
-  //   try {
-  //     var url;
-  //   if (StartTime != null) {
-  //     url = `https://vedanta.xyma.live/sensor/gettime?start=${StartTime}&end=${EndTime}&busbar=sensorModel10`;
-  //   } else {
-  //     url = `https://vedanta.xyma.live/sensor/getfullBackupsensorModel6`;
-  //   }
-  //     console.log(url);
-  //     const response = await fetch(url);
-  //     const infoVal = await response.json();
-  //     setInfoReport(infoVal);
-  //     // setIsBusBarClicked(false);
-  //   } catch (error) {
-  //     setErrorReport(error);
-  //   }
-  // };
-
-  const handleClick = (busbarNumber) => {
-    const busbarString = `sensorModel${busbarNumber}`;
-    setSelectBusbar(busbarString);
-    fetchDataBusbar6(); // Trigger the API call immediately after setting the busbar.
-    setTimeout(() => {
-          handleDownload();
-      }, 5000);
+  const fetchDataBusbar6 = async () => {
+    try {
+      var url;
+    if (StartTime != null) {
+      url = `https://vedanta.xyma.live/sensor/gettime?start=${StartTime}&end=${EndTime}&busbar=sensorModel6`;
+    } else {
+      url = `https://vedanta.xyma.live/sensor/getfullBackupsensorModel6`;
+    }
+      console.log(url);
+      const response = await fetch(url);
+      const infoVal = await response.json();
+      setInfoReport6(infoVal);
+      // setIsBusBarClicked(false);
+    //   setTimeout(() => {
+    //     handleDownload6();
+    // }, 3000);
+    const checkNotEmpty = () => {
+      if (infoReport6.length !== 0) {
+        handleDownload6();
+      } else {
+        setLoading(true);
+        setTimeout(checkNotEmpty, 100); // Adjust the interval as needed
+      }
+    };
+    // Start the polling
+    checkNotEmpty();
+    } catch (error) {
+      setErrorReport(error);
+    }
   };
+
+  const fetchDataBusbar10 = async () => {
+    try {
+      var url;
+    if (StartTime != null) {
+      url = `https://vedanta.xyma.live/sensor/gettime?start=${StartTime}&end=${EndTime}&busbar=sensorModel10`;
+    } else {
+      url = `https://vedanta.xyma.live/sensor/getfullBackupsensorModel10`;
+    }
+      console.log(url);
+      const response = await fetch(url);
+      const infoVal = await response.json();
+      setInfoReport10(infoVal);
+      // setIsBusBarClicked(false);
+    //   setTimeout(() => {
+    //     handleDownload10();
+    // }, 3000);
+    console.log("lenght", infoReport10);
+    const checkNotEmpty = () => {
+      if (infoReport10.length !== 0) {
+        handleDownload10();
+      } else {
+        setTimeout(checkNotEmpty, 100); // Adjust the interval as needed
+      }
+    };
+    // Start the polling
+    checkNotEmpty();
+    } catch (error) {
+      setErrorReport(error);
+    }
+    console.log("Button Clicked");
+  };
+
+  // const handleClick = (busbarNumber) => {
+  //   const busbarString = `sensorModel${busbarNumber}`;
+  //   setSelectBusbar(busbarString);
+  //   fetchDataBusbar6(); // Trigger the API call immediately after setting the busbar.
+  //   setTimeout(() => {
+  //         handleDownload();
+  //     }, 5000);
+  // };
 
   if (errorReport) {
     return <div>Error: {errorReport.message}</div>;
   }
-  if (!infoReport) {
-    return <div>Loading...</div>;
-  }
 
-  // for busbar
-  const dataFull = [];
-  for (let i = 0; i < infoReport.length; i++) {
+  // for busbar 6
+  const dataFull6 = [];
+  for (let i = 0; i < infoReport6.length; i++) {
     const data = [];
-    for (const key in infoReport[0]) {
+    for (const key in infoReport6[0]) {
       if (
         key !== "_id" &&
         key !== "id" &&
@@ -155,13 +201,13 @@ const Reports = () => {
     }
     let temp = [];
     for (let j = 0; j < data.length; j++) {
-      temp[j] = infoReport[i]?.[data[j]];
+      temp[j] = infoReport6[i]?.[data[j]];
     }
-    dataFull[i] = temp;
+    dataFull6[i] = temp;
   }
 
-  const cbtName = [];
-  for (const key in infoReport[0]) {
+  const cbtName6 = [];
+  for (const key in infoReport6[0]) {
     if (
       key !== "_id" &&
       key !== "id" &&
@@ -170,24 +216,81 @@ const Reports = () => {
       key !== "__v" &&
       key !== "TIME"
     ) {
-      cbtName.push(key);
+      cbtName6.push(key);
     }
   }
-  const time = [];
+  const time6 = [];
   //Getting Date
-  for (let k = 0; k < infoReport.length; k++) {
-    time[k] = infoReport[k]?.TIME;
+  for (let k = 0; k < infoReport6.length; k++) {
+    time6[k] = infoReport6[k]?.TIME;
   }
   //Adding Date on End
-  for (let l = 0; l < dataFull.length; l++) {
-    const timestamp = time;
-    dataFull[l][dataFull[l].length] = timestamp[l];
+  for (let l = 0; l < dataFull6.length; l++) {
+    const timestamp = time6;
+    dataFull6[l][dataFull6[l].length] = timestamp[l];
   }
 
-  cbtName.push("TimeStamp"); //Add TimeStamp header
+  cbtName6.push("TimeStamp"); //Add TimeStamp header
 
-  const handleDownload = () => {
-    const worksheet = XLSX.utils.aoa_to_sheet([cbtName, ...dataFull]);
+  const handleDownload6 = () => {
+    setLoading(false)
+    const worksheet = XLSX.utils.aoa_to_sheet([cbtName6, ...dataFull6]);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet 1");
+    XLSX.writeFile(workbook, "excel_data.xlsx");
+  };
+
+  // for busbar 10
+  const dataFull10 = [];
+  for (let i = 0; i < infoReport10.length; i++) {
+    const data = [];
+    for (const key in infoReport10[0]) {
+      if (
+        key !== "_id" &&
+        key !== "id" &&
+        key !== "createdAt" &&
+        key !== "updatedAt" &&
+        key !== "__v" &&
+        key !== "TIME"
+      ) {
+        data.push(key);
+      }
+    }
+    let temp = [];
+    for (let j = 0; j < data.length; j++) {
+      temp[j] = infoReport10[i]?.[data[j]];
+    }
+    dataFull10[i] = temp;
+  }
+
+  const cbtName10 = [];
+  for (const key in infoReport10[0]) {
+    if (
+      key !== "_id" &&
+      key !== "id" &&
+      key !== "createdAt" &&
+      key !== "updatedAt" &&
+      key !== "__v" &&
+      key !== "TIME"
+    ) {
+      cbtName10.push(key);
+    }
+  }
+  const time10 = [];
+  //Getting Date
+  for (let k = 0; k < infoReport10.length; k++) {
+    time10[k] = infoReport10[k]?.TIME;
+  }
+  //Adding Date on End
+  for (let l = 0; l < dataFull10.length; l++) {
+    const timestamp = time10;
+    dataFull10[l][dataFull10[l].length] = timestamp[l];
+  }
+
+  cbtName10.push("TimeStamp"); //Add TimeStamp header
+
+  const handleDownload10 = () => {
+    const worksheet = XLSX.utils.aoa_to_sheet([cbtName10, ...dataFull10]);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet 1");
     XLSX.writeFile(workbook, "excel_data.xlsx");
@@ -199,42 +302,42 @@ const Reports = () => {
     setFull(full);
   };
 
-  const handleSwitchClick = (data) => {
-    switch (data) {
-      case 1:
-        handleClick(1);
-        break;
-      case 2:
-        handleClick(2);
-        break;
-      case 3:
-        handleClick(3);
-        break;
-      case 4:
-        handleClick(4);
-        break;
-      case 5:
-        handleClick(5);
-        break;
-      case 6:
-        handleClick(6);
-        break;
-      case 7:
-        handleClick(7);
-        break;
-      case 8:
-        handleClick(8);
-        break;
-      case 9:
-        handleClick(9);
-        break;
-      case 10:
-        handleClick(10);
-        break;
-      default:
-        handleClick(6);
-    }
-  };
+  // const handleSwitchClick = (data) => {
+  //   switch (data) {
+  //     case 1:
+  //       handleClick(1);
+  //       break;
+  //     case 2:
+  //       handleClick(2);
+  //       break;
+  //     case 3:
+  //       handleClick(3);
+  //       break;
+  //     case 4:
+  //       handleClick(4);
+  //       break;
+  //     case 5:
+  //       handleClick(5);
+  //       break;
+  //     case 6:
+  //       handleClick(6);
+  //       break;
+  //     case 7:
+  //       handleClick(7);
+  //       break;
+  //     case 8:
+  //       handleClick(8);
+  //       break;
+  //     case 9:
+  //       handleClick(9);
+  //       break;
+  //     case 10:
+  //       handleClick(10);
+  //       break;
+  //     default:
+  //       handleClick(6);
+  //   }
+  // };
 
   const generateExcel = () => {
     const chunkedDataArrays = FullData.map((FullData) => {
@@ -280,7 +383,7 @@ const Reports = () => {
 
   return (
     <>
-    {infoReport.length !== 0 ? (
+    {infoReport6.length !== 0 ? (
       <div className="report">
       <div className="container">
         <div className="head"  onClick={generateExcel}>
@@ -294,10 +397,10 @@ const Reports = () => {
             className="btn"
             onClick={() => {
               setReportIsOpentime(true);
-              handleSwitchClick(1);
+              // handleSwitchClick(1);
               // handleDownload();
               setTimeout(() => {
-                handleDownload();
+                // handleDownload();
             }, 3000);
             }}
           >
@@ -307,10 +410,10 @@ const Reports = () => {
             className="btn"
             onClick={() => {
               setReportIsOpentime(true);
-              handleSwitchClick(2);
+              // handleSwitchClick(2);
               // handleDownload();
               setTimeout(() => {
-                handleDownload();
+                // handleDownload();
             }, 3000);
             }}
           >
@@ -320,10 +423,10 @@ const Reports = () => {
             className="btn"
             onClick={() => {
               setReportIsOpentime(true);
-              handleSwitchClick(3);
+              // handleSwitchClick(3);
               // handleDownload();
               setTimeout(() => {
-                handleDownload();
+                // handleDownload();
             }, 3000);
             }}
           >
@@ -333,10 +436,10 @@ const Reports = () => {
             className="btn"
             onClick={() => {
               setReportIsOpentime(true);
-              handleSwitchClick(4);
+              // handleSwitchClick(4);
               // handleDownload();
               setTimeout(() => {
-                handleDownload();
+                // handleDownload();
             }, 3000);
             }}
           >
@@ -346,10 +449,10 @@ const Reports = () => {
             className="btn"
             onClick={() => {
               setReportIsOpentime(true);
-              handleSwitchClick(5);
+              // handleSwitchClick(5);
               // handleDownload();
               setTimeout(() => {
-                handleDownload();
+                // handleDownload();
             }, 3000);
             }}
           >
@@ -358,13 +461,14 @@ const Reports = () => {
           <div
             className="btn"
             onClick={() => {
-              setReportIsOpentime(true);
-              handleSwitchClick(6);
+              fetchDataBusbar6();
+              // setReportIsOpentime(true);
+              // handleSwitchClick(6);
               // setIsBusBarClicked(true);
               // handleDownload();
-              setTimeout(() => {
-                handleDownload();
-            }, 1000);
+            //   setTimeout(() => {
+            //     handleDownload();
+            // }, 1000);
             }}
           >
             BusBar 6
@@ -378,8 +482,8 @@ const Reports = () => {
           <div
             className="btn"
             onClick={() => {
-              setReportIsOpentime(true);
-              handleSwitchClick(7);
+              // setReportIsOpentime(true);
+              // handleSwitchClick(7);
             //   setTimeout(() => {
             //     handleDownload();
             // }, 3000);
@@ -391,10 +495,10 @@ const Reports = () => {
             className="btn"
             onClick={() => {
               setReportIsOpentime(true);
-              handleSwitchClick(8);
+              // handleSwitchClick(8);
               // handleDownload();
               setTimeout(() => {
-                handleDownload();
+                // handleDownload();
             }, 3000);
             }}
           >
@@ -404,10 +508,10 @@ const Reports = () => {
             className="btn"
             onClick={() => {
               setReportIsOpentime(true);
-              handleSwitchClick(9);
+              // handleSwitchClick(9);
               // handleDownload();
               setTimeout(() => {
-                handleDownload();
+                // handleDownload();
             }, 3000);
             }}
           >
@@ -416,9 +520,10 @@ const Reports = () => {
           <div
             className="btn"
             onClick={() => {
-              setReportIsOpentime(true);
+              // setReportIsOpentime(true);
               // setIsBusBarClicked(true);
-              handleSwitchClick(10);
+              // handleSwitchClick(10);
+              fetchDataBusbar10();
             //   setTimeout(() => {
             //     handleDownload();
             // }, 5000);
